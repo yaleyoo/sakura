13d0d405505586ad92b03be31464e57403761537
Yuanyu Guo <yuanyug@student.unimelb.edu.au>
2018-09-04 15:45:16 +1000

    initial commit

---
+++ Sakura/src/servlet/MainServlet.java
@@ -0,0 +1,9 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/cart/view")
+public class MainServlet extends HttpServlet {
+
+}
