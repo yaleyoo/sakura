1367b8184446c79a6349ac3b2efc13ede4b083ac
Yuanyu Guo <yuanyug@student.unimelb.edu.au>
2018-10-04 10:51:41 +1000

    implemented staffBuildings command

---
+++ Sakura/src/servlet/StaffBuildingsCommand.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+
+import domain.Building;
+import service.BuildingService;
+
+public class StaffBuildingsCommand extends FrontCommand{
+
+	@Override
+	public void process() throws ServletException, IOException {
+		
+		BuildingService bs = new BuildingService();
+		List<Building> buildingList = bs.getAllBuildings();
+		request.setAttribute("buildingList", buildingList);
+		forward("/jsp/staff/staffBuildings.jsp");
+	}
+
+}
